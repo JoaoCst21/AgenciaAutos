5b3135c9d384794feef6219065478ca5a3cb9481
Joao <bcastillo-2022474@kinal.edu.gt>
2022-07-21 07:51:01 -0600

    last commit before dead

--- src/com/joao/view/ReadFrame.java
+++ src/com/joao/view/ReadFrame.java
@@ -1,15 +1,32 @@
 package com.joao.view;
 
-import javax.swing.JFrame;
-import javax.swing.JTable;
+import com.joao.model.Conveyance;
 
-public class ReadFrame extends JFrame {
-    public ReadFrame() {
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JTable;
+import java.util.ArrayList;
+
+public abstract class ReadFrame extends JInternalFrame {
+    String[][] data;
+    String[] column;
+    public ReadFrame(String[] column) {
+        this.column = column;
         customComponents();
         setVisible(true);
     }
 
     private void customComponents() {
-//        JTable table = new JTable();
+
+        // getting arrays
+
+        // Cars example
+
+        JTable table = new JTable(data, column);
+
+        // add table to frame
+        add((table));
     }
+
+    protected abstract void initialiazeTableProperties();
 }
