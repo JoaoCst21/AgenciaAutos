a6a19ac173f768d3c62cdfc2dbaf5c76d1ed6062
Joao <bcastillo-2022474@kinal.edu.gt>
2022-07-26 21:30:16 -0600

    comment Create and Read Frame

--- src/com/joao/view/CreateFrame.java
+++ src/com/joao/view/CreateFrame.java
@@ -1,8 +1,6 @@
 package com.joao.view;
 
-import com.joao.controller.ConveyanceController;
 import com.joao.model.CRUD;
-import com.joao.model.Conveyance;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -10,9 +8,7 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.JTextField;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -20,21 +16,55 @@ import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class CreateFrame<T, E extends CRUD> extends JInternalFrame implements ActionListener {
-    private final E controller;
+
+/**
+* JInternalFrame/Ventana customizable para crear y almaceanr Objetos de tipo {@link Class}
+ * en el {@link #controller} <br/>
+ * crea los textfields y labels en base al array de
+ * String que recibe en el constructor {@link #CreateFrame(com.joao.model.CRUD, String[])}
+ * para luego manipular los inputs con el {@link #controller} <br/><br/>
+ * @param <Class> La clase del Objeto a modificar con el {@link Controlador}
+ * @param <Controlador>> La Clase del {@link #controller} esta clase
+ * debe implementar la interface {@link com.joao.model.CRUD} con parametro del
+ * mismo tipo que {@link Class}
+ * @WARNING tomar en cuenta que en base al Array de String que se recibe en el constructor
+ * {@link com.joao.view.CreateFrame#CreateFrame(com.joao.model.CRUD, String[])}
+ * se espera que se pueda obtener todos los datos necesarios para Crear
+ * un Objeto tipo {@link Class} y luego manipularle con el controlaodor
+ * @see com.joao.view.CreateFrame#CreateFrame(com.joao.model.CRUD, String[])
+* */
+public abstract class CreateFrame<Class, Controlador extends CRUD<Class>> extends JInternalFrame implements ActionListener {
+    private final Controlador controller;
     JPanel panel;
     String[] fields;
     HashMap<String, JPanel> subPanels = new HashMap<>();
     HashMap<String, JLabel> labels = new HashMap<>();
     HashMap<String, JTextField> textfields = new HashMap<>();
 
-    public CreateFrame(E controller, String[] fields) {
+    /**
+     * @param controller Una instancia (de preferencia singleton) que debe implementar
+     * {@link com.joao.model.CRUD}. <br/>
+     * con esta se validara, y agregara objetos de tipo {@link Class} se espera que estos
+     * objetos sean almacenados en el {@link com.joao.view.CreateFrame#controller}
+     * @param fields Array con el nombre de todas las propiedades de un Objeto {@link Class} que
+     * seran usadas para crear Labels con cada uno de los valores del array e
+     * instanciar un Objeto con el input de cada label. <br/>
+     * Cada valor del array es utilizado como Key de los HashMap
+     * {@link #textfields}, {@link #labels}, {@link #subPanels}
+     * para más información de ese proceso ver {@link #setLabelsTextFields()}
+     * en alguna de las implementaciones
+     * @see #setLabelsTextFields()
+     * */
+    public CreateFrame(Controlador controller, String[] fields) {
         this.controller = controller;
         this.fields = fields;
         customComponents();
         setVisible(true);
     }
 
+    /**
+     * establece los paneles y todo lo necesario del frame
+     * */
     private void customComponents() {
         // panel
         panel = new JPanel();
@@ -63,6 +93,14 @@ public abstract class CreateFrame<T, E extends CRUD> extends JInternalFrame impl
 
 
     // Methods
+    /**
+     * Crea y centra cada una de las Labels con combre en base a cada uno de los valores
+     * {@link com.joao.view.CreateFrame#fields}
+     * puedes acceder a cada uno de las labels, textfields o subpanels con
+     * {@link com.joao.view.CreateFrame#labels}, {@link com.joao.view.CreateFrame#textfields},
+     * {@link com.joao.view.CreateFrame#subPanels} respectivamente, ingresando
+     * el nombre del field como key en el HashMap
+     * */
     private void setLabelsTextFields() {
         for (String field : fields) {
             subPanels.put(field, new JPanel());
@@ -83,19 +121,31 @@ public abstract class CreateFrame<T, E extends CRUD> extends JInternalFrame impl
         }
     }
 
+    /**
+     * Revisa que todos los TextFields estan llenos
+     * */
     private void checkTextFields() throws Exception {
         for (Map.Entry<String, JTextField> text : textfields.entrySet()) {
-            if (text.getValue().getText().equals("")) throw new Exception("You must fill all Fields");
+            if (text.getValue().getText().isEmpty()) throw new Exception("You must fill all Fields");
         }
     }
     
-    private void tryCreateCar() {
+    /**
+     * Intenta crear un Objeto de clase {@link Class}
+     * hace uso de {@link #controller} para validar todo los datos y crear el Objeto. <br/>
+     * despues de esta operacion la Ventana se cerrara. <br/>
+     * en caso de Error desplegara una Ventana {@link javax.swing.JOptionPane} con el error
+     * y no se creara ningun objeto
+     * @WARNING esta ventana se cerrará, después de esta operacion, no solo se escondera. <br/>
+     * se hace uso de {@link #dispose()} para esta operacion
+     * */
+    private void tryCreateObj() {
         try {
             // checks for not filled fields
             checkTextFields();
 
             // get Fields, validate all, and Add new Object T to controller
-            T object = getObject();
+            Class object = getObject();
             controller.validate(object);
             controller.create(object);
 
@@ -109,10 +159,17 @@ public abstract class CreateFrame<T, E extends CRUD> extends JInternalFrame impl
         }
     }
 
-    protected abstract T getObject();
+    /**
+     * Crea un Objeto de tipo {@link Class } en base a los valores escritos en los {@link #textfields}. <br/>
+     * Este metodo sera implementado distinto por cada Clase que herede e esta
+     * @IMPORTANT Este metodo deberia ser llamado despues de {@link #checkTextFields()} para asegurar que
+     * todos los campos necesarios estan llenos
+     * */
+    protected abstract Class getObject();
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        tryCreateCar();
+        tryCreateObj();
     }
 
 }
--- src/com/joao/view/DeleteFrame.java
+++ src/com/joao/view/DeleteFrame.java
@@ -11,22 +11,40 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame implements ActionListener {
-    private E controller;
+/**
+* JInternalFrame/Ventana customizable para eliminar Objetos de tipo {@link Class}
+ * almacenados en el {@link #controller} especificado en el constructor
+ * {@link #DeleteFrame(com.joao.model.CRUD, String[])}. <br/>
+ * crea las columnas de la tabla a desplegar en base al array de
+ * String que recibe en el Constructor.
+ * @param <Class> La clase del Objeto a Eliminar del {@link #controller}
+ * @param <Controlador>> La Clase del {@link #controller} esta clase
+ * debe implementar la interface {@link com.joao.model.CRUD} con parametro del
+ * mismo tipo que {@link Class}
+* */
+public abstract class DeleteFrame<Class, Controlador extends CRUD<Class>> extends JInternalFrame implements ActionListener {
+    private Controlador controller;
     String[][] data;
-    T object;
+    Class object;
     String[] column;
     private JLabel labelBuscar;
     private JTextField textfieldBuscar;
     private JButton buttonSearch;
     private JButton buttonDelete = new JButton("Delete");
 
-    public DeleteFrame(E controller, String[] column) {
+    /**
+     * @param controller Una instancia (de preferencia singleton) que debe implementar
+     * {@link com.joao.model.CRUD}. <br/>
+     * con esta se validara, recibira y eliminara objetos de tipo {@link Class} se espera que estos
+     * objetos esten almacenados en el {@link #controller}
+     * @param column Array con el nombre de todas las propiedades de un Objeto {@link Class} que
+     * seran usadas para crear los nombres de las columnas de la tabla
+     * */
+    public DeleteFrame(Controlador controller, String[] column) {
         data = new String[1][column.length];
         this.controller = controller;
         this.column = column;
@@ -34,6 +52,9 @@ public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame i
         setVisible(true);
     }
 
+    /**
+     * establece los paneles y todo lo necesario del frame
+     * */
     private void customComponents() {
        JPanel panel = getPanel();
        add(panel);
@@ -43,6 +64,9 @@ public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame i
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    /**
+     * Crea un panel con un text-field para ingresar y un botón para buscar
+     * */
     private JPanel getPanel() {
          // buscar
         JPanel panel = new JPanel(new GridLayout(2, 1, 10, 10));
@@ -67,6 +91,18 @@ public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame i
         return panel;
     }
 
+    /**
+     * maneja los eventos de {@link #buttonSearch} y {@link #buttonDelete} <br/>
+     *
+     * @buttonSerch verifica que el ID ingresado sea valido y exista en el {@link #controller},
+     * luego despliega una tabla  listando las propiedades del objeto almacenado em
+     * {@link #controller}. <br/>
+     * en caso de error despliega una Ventana explicando el error y no despliega ninguna tabla
+     * @buttonDelete llama a {@link #tryDeleteObject()} e intenta eliminar el objeto con el ID
+     * indicado del {@link #controller}
+     * y luego despliega una ventana preguntando si desea eliminarlo. <br/>
+     * en caso de error en el proceso se desplagara una ventana explicando el error y no elimina nada
+     * */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == buttonSearch) {
@@ -88,32 +124,11 @@ public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame i
             }
 
             this.getContentPane().removeAll();
-
-            // panel
-            JPanel panelTable = new JPanel(new GridLayout(2,1, 10, 30));
-
-            // Table
-            JTable table = new JTable(data, column);
-            table.setBounds(30, 40, 200, 300);
-            table.setPreferredSize(new Dimension(getSize().width, 15));
-            table.setPreferredScrollableViewportSize(new Dimension(getSize().width - 20, 15));
-
-            // ScrollPane
-            JScrollPane sp =new JScrollPane(table);
-
-            // button delete action listener
-            JPanel panelButton = new JPanel();
-            panelButton.add(buttonDelete);
-            buttonDelete.addActionListener(this);
-
-            // add
-            panelTable.add(sp);
-            panelTable.add(panelButton);
-
             // Panel all
             JPanel panelParent = new JPanel();
 
-            panelParent.add(panelTable);
+            // ad to panel
+            panelParent.add(getTable());
             panelParent.add(getPanel());
 
             add(panelParent);
@@ -123,7 +138,39 @@ public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame i
         if (e.getSource() == buttonDelete) tryDeleteObject();
 
     }
+    /**
+     * Crea una Tabla con los valores del array {@link #column} como columnas
+     * y un boton para eliminar el objeto mostrado del {@link #controller}
+     * */
+    private JPanel getTable() {
+        // panel
+        JPanel panelTable = new JPanel(new GridLayout(2,1, 10, 30));
 
+        // Table
+        JTable table = new JTable(data, column);
+        table.setBounds(30, 40, 200, 300);
+        table.setPreferredSize(new Dimension(getSize().width, 15));
+        table.setPreferredScrollableViewportSize(new Dimension(getSize().width - 20, 15));
+
+        // ScrollPane
+        JScrollPane sp =new JScrollPane(table);
+
+        // button delete action listener
+        JPanel panelButton = new JPanel();
+        panelButton.add(buttonDelete);
+        buttonDelete.addActionListener(this);
+
+        // add
+        panelTable.add(sp);
+        panelTable.add(panelButton);
+        return panelTable;
+    }
+
+    /**
+     * valida e intenta eliminar el valor actual de {@link #object} de {@link #controller}
+     * en caso de error desplega una ventana explicando el error y cancela el proceso
+     * de eliminación
+     * */
     private void tryDeleteObject() {
         try {
             // checks for not filled fields
@@ -141,7 +188,15 @@ public abstract class DeleteFrame<T, E extends CRUD<T>> extends JInternalFrame i
             JOptionPane.showMessageDialog(null, ex.getMessage(), "Error Deleting", JOptionPane.ERROR_MESSAGE);
         }
     }
-    protected abstract void setDataTableValue(T obj);
+
+    /**
+     * Deconstruye el Objeto recibido y muestra sus propiedades en la tabla
+     * */
+    protected abstract void setDataTableValue(Class obj);
+
+    /**
+     * Verifica que todos los Campos requeridos necesarios esten completos
+     * */
     private void checkTextFields() throws Exception {
             if (labelBuscar.getText().equals("")) throw new Exception("You must fill all Fields");
     }
